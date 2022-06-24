8c469289c7794b4525da2edbe459c3854bde9dbe
Shaan NARAIN <snarain@e2r11p13.42.fr>
2022-06-24 19:39:16 +0200

    push 02

---
+++ cell03/ex00/background.js
@@ -0,0 +1,7 @@
+function random_color() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + r + "," + g + "," + b + ")";
+    document.body.style.backgroundColor = bgColor;
+}
